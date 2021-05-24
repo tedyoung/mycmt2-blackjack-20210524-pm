7d431805c6f50a96796bc959c1ea31c155d47d85
Ted M. Young <tedyoung@gmail.com>
2021-05-24 16:08:39 -0700

    Lab 2: Moved String-based value display method out of Hand, tidied up Hand and removed valueEquals as it's no longer needed.

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -17,7 +17,7 @@ public class Hand {
         return cards.get(0);
     }
 
-    private int value() {
+    public int value() {
         int handValue = cards
                 .stream()
                 .mapToInt(Card::rankValue)
@@ -62,11 +62,4 @@ public class Hand {
         return hand.value() < value();
     }
 
-    String displayValue() {
-        return String.valueOf(value());
-    }
-
-    public boolean valueEquals(int target) {
-        return value() == target;
-    }
 }
