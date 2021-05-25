ca274dc0aba3d283b522c8f6d1e84ff940af733b
Ted M. Young <tedyoung@gmail.com>
2021-05-25 14:37:39 -0700

    Lab #3: determineOutcome returns a String no longer displaying to console.

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
@@ -9,59 +9,59 @@ import static org.fusesource.jansi.Ansi.ansi;
 
 public class ConsoleGame {
 
-  private final Game game;
+    private final Game game;
 
-  public ConsoleGame(Game game) {
-    this.game = game;
-  }
+    public ConsoleGame(Game game) {
+        this.game = game;
+    }
 
-  public static void resetScreen() {
-      System.out.println(ansi().reset());
-  }
+    public static void resetScreen() {
+        System.out.println(ansi().reset());
+    }
 
-  public static void displayWelcomeScreen() {
-      System.out.println(ansi()
-                                 .bgBright(Ansi.Color.WHITE)
-                                 .eraseScreen()
-                                 .cursor(1, 1)
-                                 .fgGreen().a("Welcome to")
-                                 .fgRed().a(" Jitterted's")
-                                 .fgBlack().a(" BlackJack"));
-  }
+    public static void displayWelcomeScreen() {
+        System.out.println(ansi()
+                                   .bgBright(Ansi.Color.WHITE)
+                                   .eraseScreen()
+                                   .cursor(1, 1)
+                                   .fgGreen().a("Welcome to")
+                                   .fgRed().a(" Jitterted's")
+                                   .fgBlack().a(" BlackJack"));
+    }
 
-  public static String inputFromPlayer() {
-      System.out.println("[H]it or [S]tand?");
-      Scanner scanner = new Scanner(System.in);
-      return scanner.nextLine();
-  }
+    public static String inputFromPlayer() {
+        System.out.println("[H]it or [S]tand?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
 
-  public static void displayGameState(Game game) {
-      System.out.print(ansi().eraseScreen().cursor(1, 1));
-      System.out.println("Dealer has: ");
-      System.out.println(ConsoleHand.displayFirstCard(game.dealerHand())); // first card is Face Up
+    public static void displayGameState(Game game) {
+        System.out.print(ansi().eraseScreen().cursor(1, 1));
+        System.out.println("Dealer has: ");
+        System.out.println(ConsoleHand.displayFirstCard(game.dealerHand())); // first card is Face Up
 
-      // second card is the hole card, which is hidden
-      displayBackOfCard();
+        // second card is the hole card, which is hidden
+        displayBackOfCard();
 
-      System.out.println();
-      System.out.println("Player has: ");
-      System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
-      System.out.println(" (" + game.playerHand().value() + ")");
-  }
+        System.out.println();
+        System.out.println("Player has: ");
+        System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
+        System.out.println(" (" + game.playerHand().value() + ")");
+    }
 
-  private static void displayBackOfCard() {
-      System.out.print(
-              ansi()
-                      .cursorUp(7)
-                      .cursorRight(12)
-                      .a("┌─────────┐").cursorDown(1).cursorLeft(11)
-                      .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                      .a("│░ J I T ░│").cursorDown(1).cursorLeft(11)
-                      .a("│░ T E R ░│").cursorDown(1).cursorLeft(11)
-                      .a("│░ T E D ░│").cursorDown(1).cursorLeft(11)
-                      .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                      .a("└─────────┘"));
-  }
+    private static void displayBackOfCard() {
+        System.out.print(
+                ansi()
+                        .cursorUp(7)
+                        .cursorRight(12)
+                        .a("┌─────────┐").cursorDown(1).cursorLeft(11)
+                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
+                        .a("│░ J I T ░│").cursorDown(1).cursorLeft(11)
+                        .a("│░ T E R ░│").cursorDown(1).cursorLeft(11)
+                        .a("│░ T E D ░│").cursorDown(1).cursorLeft(11)
+                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
+                        .a("└─────────┘"));
+    }
 
     public static void displayFinalGameState(Game game) {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
@@ -76,35 +76,35 @@ public class ConsoleGame {
     }
 
     public void start() {
-    displayWelcomeScreen();
+        displayWelcomeScreen();
 
-    game.initialDeal();
+        game.initialDeal();
 
-    playerPlays();
+        playerPlays();
 
-    game.dealerTurn();
+        game.dealerTurn();
 
-    displayFinalGameState(game);
+        displayFinalGameState(game);
 
-    game.determineOutcome();
+        System.out.println(game.determineOutcome());
 
-    resetScreen();
-  }
-
-  public void playerPlays() {
-    while (!game.isPlayerDone()) {
-      displayGameState(game);
-      String command = inputFromPlayer();
-      handle(command);
+        resetScreen();
     }
-  }
 
-  public void handle(String command) {
-    if (command.toLowerCase().startsWith("h")) {
-      game.playerHits();
-    } else if (command.toLowerCase().startsWith("s")) {
-      game.playerStands();
+    public void playerPlays() {
+        while (!game.isPlayerDone()) {
+            displayGameState(game);
+            String command = inputFromPlayer();
+            handle(command);
+        }
     }
-  }
+
+    public void handle(String command) {
+        if (command.toLowerCase().startsWith("h")) {
+            game.playerHits();
+        } else if (command.toLowerCase().startsWith("s")) {
+            game.playerStands();
+        }
+    }
 
 }
