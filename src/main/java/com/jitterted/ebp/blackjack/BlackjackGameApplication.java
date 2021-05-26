64b75b51d026d92ebd0ef09a63b11735d7b2920b
Ted M. Young <tedyoung@gmail.com>
2021-05-26 13:53:39 -0700

    Start game endpoint does an initial deal. Spring is now aware of how to create a DOMAIN class (Game).

--- src/main/java/com/jitterted/ebp/blackjack/BlackjackGameApplication.java
+++ src/main/java/com/jitterted/ebp/blackjack/BlackjackGameApplication.java
@@ -1,13 +1,22 @@
 package com.jitterted.ebp.blackjack;
 
+import com.jitterted.ebp.blackjack.domain.Deck;
+import com.jitterted.ebp.blackjack.domain.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlackjackGameApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BlackjackGameApplication.class, args);
+  }
+
+  // allows Spring to assemble classes from our domain without polluting it
+  @Bean
+  public Game createGame() {
+    return new Game(new Deck());
   }
 
 }
